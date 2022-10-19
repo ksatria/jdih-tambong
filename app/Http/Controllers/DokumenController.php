df8cee4dfed5336489d14fb74b1c8978603263a3
Khoirul Umam <khoirul.umam35@gmail.com>
2022-10-20 06:26:16 +0700

    Inisialisasi Fitur Jenis Dokumen
    Fitur untuk menampilkan daftar dokumen berdasarkan jenisnya pada halaman yang berbeda telah dikembangkan

--- app/Http/Controllers/DokumenController.php
+++ app/Http/Controllers/DokumenController.php
@@ -7,10 +7,12 @@ use App\Models\Dokumen;
 
 class DokumenController extends Controller
 {
+    private $JUMLAH_DOKUMEN_PER_HALAMAN = 10;
+
     function index()
     {
         $dokumenTerbaru = Dokumen::orderBy('tanggal_pengesahan', 'desc')
-            ->limit(10)
+            ->limit(5)
             ->get();
 
         return view('beranda', ["kumpulanDokumen" => $dokumenTerbaru]);
@@ -18,26 +20,59 @@ class DokumenController extends Controller
 
     function semuaDokumen()
     {
+        /* --- TO DO --- */
+        // $pagination = Dokumen::orderBy('tanggal_pengesahan', 'desc')
+        //     ->paginate($this->JUMLAH_DOKUMEN_PER_HALAMAN);
+
+        $dokumen = Dokumen::orderBy('tanggal_pengesahan', 'desc')->get();
+
+        $data = [
+            "deskripsiHalaman" => "Kumpulan semua dokumen hukum di lingkungan Desa Tambong, Kabat, Banyuwangi, Jawa Timur",
+            "judulHalaman" => "Dokumen Hukum",
+            "kumpulanDokumen" => $dokumen
+        ];
+
+        return view('dokumen', $data);
     }
 
     function perdes($id = null, $title = null)
     {
+        return $this->viewJenisDokumen('Perdes', 'Peraturan Desa');
     }
 
     function perkades($id = null, $title = null)
     {
+        return $this->viewJenisDokumen('Perkades', 'Peraturan Kepala Desa');
     }
 
     function permakades($id = null, $title = null)
     {
+        return $this->viewJenisDokumen('Permakades', 'Peraturan Bersama Kepala Desa');
     }
 
     function keputusan($id = null, $title = null)
     {
+        return $this->viewJenisDokumen('SK Kades', 'Surat Keputusan Kepala Desa');
     }
 
     function dokumenLain($id = null, $title = null)
     {
+        return $this->viewJenisDokumen('Lain', 'Dokumen Hukum Lainnya');
+    }
+
+    private function viewJenisDokumen($singkatan, $kepanjangan)
+    {
+        $dokumen = Dokumen::whereRelation('tipeDokumen', 'singkatan_tipe', $singkatan)
+            ->orderBy('tanggal_pengesahan', 'desc')
+            ->get();
+
+        $data = [
+            "deskripsiHalaman" => "Kumpulan {$kepanjangan} yang diterbitkan di lingkungan Desa Tambong, Kabat, Banyuwangi, Jawa Timur",
+            "judulHalaman" => $kepanjangan,
+            "kumpulanDokumen" => $dokumen
+        ];
+
+        return view('dokumen', $data);
     }
 
     function cariDokumen(Request $request)
---
+++ resources/views/dokumen.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.utama');
+
+@section('meta.deskripsi', $deskripsiHalaman)
+@section('judul', $judulHalaman)
+
+@section('konten-utama')
+    <h1 class="display-6 fw-semibold my-5">{{ $judulHalaman }}</h1>
+
+    <x-form-pencarian />
+
+    @if (count($kumpulanDokumen) === 0)
+        <p><em>Tidak ditemukan dokumen terkait</em></p>
+    @else
+        @foreach ($kumpulanDokumen as $dokumen)
+            <x-dokumen.card :dokumen="$dokumen" />
+        @endforeach
+    @endif
+@endsection
