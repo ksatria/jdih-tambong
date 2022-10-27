99ff3f6bba32acdd56a9cdf482f25d48a0a4b8b4
Khoirul Umam <khoirul.umam35@gmail.com>
2022-10-27 11:14:11 +0700

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
-<div class="container">
-    <footer id="page-footer" class="border-top py-3 my-4 text-muted">
+<footer id="page-footer" class="bg-dark text-bg-dark mt-4">
+    <div class="container border-top py-3">
         <div><small class="fw-semibold">Website JDIH Desa Tambong | Prototype #1</small></div>
         <div><small>Dikembangkan oleh Tim P2DT Desa Tambong Tahun 2022</small></div>
         <div><small>Politeknik Negeri Banyuwangi</small></div>
-    </footer>
-</div>
+    </div>
+</footer>
