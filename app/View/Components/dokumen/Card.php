e348f2a42b9c991a932349d1450e76c8e6381992
Khoirul Umam <khoirul.umam35@gmail.com>
2022-11-19 23:24:40 +0700

    Update Card.php
    Penyesuaian adanya helper format tanggal

--- app/View/Components/dokumen/Card.php
+++ app/View/Components/dokumen/Card.php
@@ -19,13 +19,6 @@ class Card extends Component
     public $jumlahDilihat;
     public $jumlahDidownload;
 
-    private $bulan = [
-        "Januari", "Februari", "Maret",
-        "April", "Mei", "Juni",
-        "Juli", "Agustus", "September",
-        "Oktober", "November", "Desember"
-    ];
-
     /**
      * Create a new component instance.
      *
