c895804d375030fd3043fc58aad76754ccd226ac
Khoirul Umam <khoirul.umam35@gmail.com>
2022-11-19 22:54:40 +0700

    Update Detail.php
    Daftar nama bulan sudah dipindah ke helper format tanggal

--- app/View/Components/dokumen/Detail.php
+++ app/View/Components/dokumen/Detail.php
@@ -19,13 +19,6 @@ class Detail extends Component
     public $berkasTerkait;
     public $dokumenTerkait;
 
-    private $bulan = [
-        "Januari", "Februari", "Maret",
-        "April", "Mei", "Juni",
-        "Juli", "Agustus", "September",
-        "Oktober", "November", "Desember"
-    ];
-
     /**
      * Create a new component instance.
      *
